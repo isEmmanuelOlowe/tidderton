fc6ae972d5d9be3c41b555fccb5db60f92508915
Emmanuel Olowe <30698194+isEmmanuelOlowe@users.noreply.github.com>
2021-02-11 20:03:40 +0000

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
 module.exports = {
     env: {
-      BACKEND_HOST: 'https://cs3099user-b6.host.cs.st-andrews.ac.uk/',
+      BACKEND_HOST: 'https://cs3099user-b6.host.cs.st-andrews.ac.uk',
     },
   }
